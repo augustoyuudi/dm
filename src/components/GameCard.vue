11aec9664fa3ab5f28ed7d3c3a64f4f658ba35e4
Augusto Yuudi Shiraishi <augustoyuudi@gmail.com>
2021-09-20 02:16:22 -0300

    feat: create GameCard.vue component

---
+++ src/components/GameCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class='base-card'>
+    <picture
+      class="base-card__image-container"
+    >
+      <source
+        srcset='@/assets/images/game-cover.jpeg'
+      >
+      <img
+        class="base-card__image"
+        src='@/assets/images/game-cover.jpeg'
+        :alt='`Cover of pokemon game called ${game.name}`'
+      >
+    </picture>
+    <aside class="base-card__content">
+      <h4 class="base-card__title">Pokémon <br>{{ capitalizedGameName }}</h4>
+      <h5 class="base-card__subtitle">#{{ game.id }}</h5>
+    </aside>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    game: {
+      type: Object,
+    },
+  },
+
+  computed: {
+    capitalizedGameName() {
+      if (!this.game) {
+        return '';
+      }
+
+      const firstLetter = this.game.name.charAt(0).toUpperCase();
+
+      return `${firstLetter}${this.game.name.slice(1)}`;
+    },
+  },
+};
+</script>
