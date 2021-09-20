639fbd876e19504efa8e19c51546a06a03298b1f
Augusto Yuudi Shiraishi <augustoyuudi@gmail.com>
2021-09-20 20:19:37 -0300

    feat: create loader component

---
+++ src/components/shared/Loader.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="loader">
+    <div class="loader__loading">
+      <div class="loader__ball"></div>
+      <div class="loader__text">NOW LOADING</div>
+    </div>
+  </div>
+</template>
