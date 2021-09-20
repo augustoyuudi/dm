1527f85478f9c7674a7972d218194fea79989145
Augusto Yuudi Shiraishi <augustoyuudi@gmail.com>
2021-09-20 20:19:53 -0300

    feat: use loader component in Game view

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,36 +1,44 @@
 <template>
   <main class="game view">
-    <picture class="game__cover">
-      <source srcset="@/assets/images/game-cover.jpeg">
-      <img src="@/assets/images/game-cover.jpeg">
-    </picture>
-    <section v-if="game" class='game__details'>
-      <div class="game__details__border">
-        <div class="game__details__background">
-          <p>Name: {{ game.name }} {{ game.names[0].name }}</p>
-          <p>Generation: {{ game.generation.name }}</p>
-          <p>Regions: {{ game.regions[0].name }}</p>
-          <p>Pokedexes: {{ game.pokedexes[0].name }}</p>
-          <details class="game__details__pokemons">
-            <summary>Available pokémons</summary>
-            <p
-              v-for="pokemon in game.pokedex.pokemon_entries"
-              :key='pokemon.entry_number'
-            >
-              {{ pokemon.pokemon_species.name }}
-            </p>
-          </details>
+    <loader v-show='!game'></loader>
+    <div v-show="game">
+      <picture class="game__cover">
+        <source srcset="@/assets/images/game-cover.jpeg">
+        <img src="@/assets/images/game-cover.jpeg">
+      </picture>
+      <section v-if="game" class='game__details'>
+        <div class="game__details__border">
+          <div class="game__details__background">
+            <p>Name: {{ game.name }} {{ game.names[0].name }}</p>
+            <p>Generation: {{ game.generation.name }}</p>
+            <p>Regions: {{ game.regions[0].name }}</p>
+            <p>Pokedexes: {{ game.pokedexes[0].name }}</p>
+            <details class="game__details__pokemons">
+              <summary>Available pokémons</summary>
+              <p
+                v-for="pokemon in game.pokedex.pokemon_entries"
+                :key='pokemon.entry_number'
+              >
+                {{ pokemon.pokemon_species.name }}
+              </p>
+            </details>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   </main>
 </template>
 
 <script>
 import GamesAPI from '@/services/http/Games';
+import Loader from '@/components/shared/Loader.vue';
 
 export default {
   name: 'Game',
+
+  components: {
+    Loader,
+  },
 
   props: {
     gameName: {
